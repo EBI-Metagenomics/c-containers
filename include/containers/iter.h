35ab2fae43dc86f331f4bfde8f989a2460c2a48e
Danilo Horta <danilo.horta@pm.me>
2021-05-04 11:29:33 +0100

    0.0.3

--- include/containers/iter.h
+++ include/containers/iter.h
@@ -9,22 +9,33 @@ struct iter {
   struct node const *end;
 };
 
-static inline struct node *iter_node_next(struct iter *iter) {
-  if (iter->curr == iter->end)
+static inline struct node *iter_next(struct iter *iter) {
+  if (!iter || iter->curr == iter->end)
     return NULL;
   struct node *node = iter->curr;
   iter->curr = node->next;
   return node;
 }
 
-#define iter_next(x) _Generic((x), struct iter * : iter_node_next)(x)
+#define iter_entry(pos, type, member) container_of_safe(pos, type, member)
+
+#define iter_next_entry(iter, entry, member)                                   \
+  container_of_safe(iter_next(iter), __typeof__(*entry), member)
 
 #define iter_for_each(pos, iter, member)                                       \
   for (pos = iter_next(iter); pos; pos = iter_next(iter))
 
+#define iter_for_each_safe(pos, tmp, iter, member)                             \
+  for (pos = iter_next(iter), tmp = iter_next(iter); pos;                      \
+       pos = tmp, tmp = iter_next(iter))
+
 #define iter_for_each_entry(entry, iter, member)                               \
-  for (entry = container_of_safe(iter_next(iter), __typeof__(*entry), member); \
-       entry;                                                                  \
-       entry = container_of_safe(iter_next(iter), __typeof__(*entry), member))
+  for (entry = iter_next_entry(iter, entry, member); entry;                    \
+       entry = iter_next_entry(iter, entry, member))
+
+#define iter_for_each_entry_safe(entry, tmp, iter, member)                     \
+  for (entry = iter_next_entry(iter, entry, member),                           \
+      tmp = iter_next_entry(iter, entry, member);                              \
+       entry; entry = tmp, tmp = iter_next_entry(iter, entry, member))
 
 #endif
